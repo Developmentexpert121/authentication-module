6a63ebf022bfc712f19534c662f067b0c6ac6af0
developmentexpert121@gmail.com <developmentexpert121@gmail.com>
2025-09-25 11:12:54 +0530

    backend link update

--- frontend/src/component/FieldSelector.tsx
+++ frontend/src/component/FieldSelector.tsx
@@ -113,7 +113,7 @@ export default function FieldSelector() {
     backend: sortFields(mergedBackend),
   };
 
-    const response = await fetch('http://localhost:5000/generate', {
+    const response = await fetch('https://authentication-module-dqtd.onrender.com/generate', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ db: dbChoice, auth: authType, fields: allFields }),
